98d07d19cf80f8f68927a860c7e3c3bf8ce1821c
SushreeP <pratishruti.mohanty7@gmail.com>
2022-07-01 15:42:42 +0100

    app chsnged to stateless

--- src/App.js
+++ src/App.js
@@ -1,113 +1,118 @@
-import React, { Component } from 'react';
-import { Container } from 'reactstrap';
-import { getTokenOrRefresh } from './token_util';
-import './custom.css'
-import { ResultReason } from 'microsoft-cognitiveservices-speech-sdk';
+import React, { useState, useEffect } from "react";
+import { Container } from "reactstrap";
+import { getTokenOrRefresh } from "./token_util";
+import "./custom.css";
+import { ResultReason } from "microsoft-cognitiveservices-speech-sdk";
 
-const speechsdk = require('microsoft-cognitiveservices-speech-sdk')
+const speechsdk = require("microsoft-cognitiveservices-speech-sdk");
 
+const App = () => {
+  const [displayText, setDisplayText] = useState(
+    "INITIALIZED: ready to test speech..."
+  );
 
-export default class App extends Component {
-    constructor(props) {
-        super(props);
+  useEffect(() => {
+    const fetchData = async () => {
+      // check for valid speech key/region
+      const tokenRes = await getTokenOrRefresh();
+      if (tokenRes.authToken === null) {
+        setDisplayText("FATAL_ERROR: " + tokenRes.error);
+      }
+    };
+    fetchData();
+  }, []);
 
-        this.state = {
-            displayText: 'INITIALIZED: ready to test speech...'
-        }
-    }
-    
-    async componentDidMount() {
-        // check for valid speech key/region
-        const tokenRes = await getTokenOrRefresh();
-        if (tokenRes.authToken === null) {
-            this.setState({
-                displayText: 'FATAL_ERROR: ' + tokenRes.error
-            });
-        }
-    }
+  const sttFromMic = async () => {
+    const tokenObj = await getTokenOrRefresh();
+    const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(
+      tokenObj.authToken,
+      tokenObj.region
+    );
+    speechConfig.speechRecognitionLanguage = "en-US";
 
-    async sttFromMic() {
-        const tokenObj = await getTokenOrRefresh();
-        const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(tokenObj.authToken, tokenObj.region);
-        speechConfig.speechRecognitionLanguage = 'en-US';
-        
-        const audioConfig = speechsdk.AudioConfig.fromDefaultMicrophoneInput();
-        const recognizer = new speechsdk.SpeechRecognizer(speechConfig, audioConfig);
+    const audioConfig = speechsdk.AudioConfig.fromDefaultMicrophoneInput();
+    const recognizer = new speechsdk.SpeechRecognizer(
+      speechConfig,
+      audioConfig
+    );
 
-        this.setState({
-            displayText: 'speak into your microphone...'
-        });
+    setDisplayText("speak into your microphone...");
 
-        recognizer.recognizeOnceAsync(result => {
-            let displayText;
-            if (result.reason === ResultReason.RecognizedSpeech) {
-                displayText = `RECOGNIZED: Text=${result.text}`
-            } else {
-                displayText = 'ERROR: Speech was cancelled or could not be recognized. Ensure your microphone is working properly.';
-            }
+    recognizer.recognizeOnceAsync((result) => {
+      let displayText;
+      if (result.reason === ResultReason.RecognizedSpeech) {
+        displayText = `RECOGNIZED: Text=${result.text}`;
+      } else {
+        displayText =
+          "ERROR: Speech was cancelled or could not be recognized. Ensure your microphone is working properly.";
+      }
+      setDisplayText(displayText);
+    });
+  };
 
-            this.setState({
-                displayText: displayText
-            });
-        });
-    }
+  const fileChange = async (event) => {
+    const audioFile = event.target.files[0];
+    console.log(audioFile);
+    const fileInfo = audioFile.name + ` size=${audioFile.size} bytes `;
 
-    async fileChange(event) {
-        const audioFile = event.target.files[0];
-        console.log(audioFile);
-        const fileInfo = audioFile.name + ` size=${audioFile.size} bytes `;
+    setDisplayText(fileInfo);
 
-        this.setState({
-            displayText: fileInfo
-        });
+    const tokenObj = await getTokenOrRefresh();
+    const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(
+      tokenObj.authToken,
+      tokenObj.region
+    );
+    speechConfig.speechRecognitionLanguage = "en-US";
 
-        const tokenObj = await getTokenOrRefresh();
-        const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(tokenObj.authToken, tokenObj.region);
-        speechConfig.speechRecognitionLanguage = 'en-US';
+    const audioConfig = speechsdk.AudioConfig.fromWavFileInput(audioFile);
+    const recognizer = new speechsdk.SpeechRecognizer(
+      speechConfig,
+      audioConfig
+    );
 
-        const audioConfig = speechsdk.AudioConfig.fromWavFileInput(audioFile);
-        const recognizer = new speechsdk.SpeechRecognizer(speechConfig, audioConfig);
+    recognizer.recognizeOnceAsync((result) => {
+      let displayText;
+      if (result.reason === ResultReason.RecognizedSpeech) {
+        displayText = `RECOGNIZED: Text=${result.text}`;
+      } else {
+        displayText =
+          "ERROR: Speech was cancelled or could not be recognized. Ensure your microphone is working properly.";
+      }
 
-        recognizer.recognizeOnceAsync(result => {
-            let displayText;
-            if (result.reason === ResultReason.RecognizedSpeech) {
-                displayText = `RECOGNIZED: Text=${result.text}`
-            } else {
-                displayText = 'ERROR: Speech was cancelled or could not be recognized. Ensure your microphone is working properly.';
-            }
+      setDisplayText(fileInfo + displayText);
+    });
+  };
 
-            this.setState({
-                displayText: fileInfo + displayText
-            });
-        });
-    }
+  return (
+    <Container className="app-container">
+      <h1 className="display-4 mb-3">Speech sample app</h1>
 
-    render() {
-        return (
-            <Container className="app-container">
-                <h1 className="display-4 mb-3">Speech sample app</h1>
+      <div className="row main-container">
+        <div className="col-6">
+          <i
+            className="fas fa-microphone fa-lg mr-2"
+            onClick={() => sttFromMic()}
+          ></i>
+          Convert speech to text from your mic.
+          <div className="mt-2">
+            <label htmlFor="audio-file">
+              <i className="fas fa-file-audio fa-lg mr-2"></i>
+            </label>
+            <input
+              type="file"
+              id="audio-file"
+              onChange={(e) => fileChange(e)}
+              style={{ display: "none" }}
+            />
+            Convert speech to text from an audio file.
+          </div>
+        </div>
+        <div className="col-6 output-display rounded">
+          <code>{displayText}</code>
+        </div>
+      </div>
+    </Container>
+  );
+};
 
-                <div className="row main-container">
-                    <div className="col-6">
-                        <i className="fas fa-microphone fa-lg mr-2" onClick={() => this.sttFromMic()}></i>
-                        Convert speech to text from your mic.
-
-                        <div className="mt-2">
-                            <label htmlFor="audio-file"><i className="fas fa-file-audio fa-lg mr-2"></i></label>
-                            <input 
-                                type="file" 
-                                id="audio-file" 
-                                onChange={(e) => this.fileChange(e)} 
-                                style={{display: "none"}} 
-                            />
-                            Convert speech to text from an audio file.
-                        </div>
-                    </div>
-                    <div className="col-6 output-display rounded">
-                        <code>{this.state.displayText}</code>
-                    </div>
-                </div>
-            </Container>
-        );
-    }
-}
+export default App;
